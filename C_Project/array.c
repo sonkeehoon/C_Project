b912d648fa2ec4c006b46d8df11c9500991ff419
djfkfk12345 <djfkfk12345@DESKTOP-229IEJ0>
2022-03-28 14:05:30 +0900

    초기 커밋.

---
+++ C_Project/array.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main_array(void) {
+	// 배열
+	//int subway_1 = 30; // 지하철 1호차에 30명
+	//int subway_2 = 40;
+	//int subway_3 = 50;
+	//printf("지하철 1호차에 %d 명이 타고 있습니다\n", subway_1);
+	//printf("지하철 2호차에 %d 명이 타고 있습니다\n", subway_2);
+	//printf("지하철 3호차에 %d 명이 타고 있습니다\n", subway_3);
+	
+	// 여러 개의 변수를 함께,동시에 생성
+	/*int subway_array[3];
+	subway_array[0] = 30;
+	subway_array[1] = 40;
+	subway_array[2] = 50;
+
+	for (int i = 0; i < 3; i++) {
+		printf("지하철 %d호 차에 %d 명이 타고 있습니다\n", i + 1, subway_array[i]);
+	}*/
+
+	// 값은 초기화를 반드시 해야 함
+	/*int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	for (int i = 0; i < 10; i++) {
+		printf("%d\n", arr[i]);
+	}*/
+	
+	// 배열 크기는 항상 상수로 선언
+	/*int size = 10;
+	int arr[10];*/
+
+	//int arr[10] = { 1,2 }; // 3 번째 값부터는 자동으로 '0'으로 초기화
+	//for (int i = 0; i < 10; i++) {
+	//	printf("%d\n", arr[i]);
+	//}
+
+	int arr[] = { 1,2 }; // arr[2]
+	float arr_f[5] = { 1.0f,2.0f,3.0f };
+	for (int i = 0; i < 5; i++) {
+		printf("%.2f\n", arr_f[i]);
+	}
+
+	return 0;
+}
